4d0a6df310c8634cf5860ae427a2da0d38ce052a
Jack Yu <jackyu@google.com>
2019-10-15 17:20:29 -0700

    When SPN is empty, use PNN in mobile network settings
    Added one more name source 'PNN', which has priority
    higher than carrier id but lower than EF_SPN.
    
    Bug: 143493866
    Test: Manual
    
    Change-Id: Ide7f60e504c73994ddc6aebf23d2f5c02926d44f

--- src/java/com/android/internal/telephony/SubscriptionController.java
+++ src/java/com/android/internal/telephony/SubscriptionController.java
@@ -1558,13 +1558,14 @@ public class SubscriptionController extends ISub.Stub {
      * @return int representing the priority. Higher value means higher priority.
      */
     public static int getNameSourcePriority(@SimDisplayNameSource int nameSource) {
-        int index = Arrays.asList(
-                SubscriptionManager.NAME_SOURCE_DEFAULT_SOURCE,
-                SubscriptionManager.NAME_SOURCE_SIM_PNN,
-                SubscriptionManager.NAME_SOURCE_SIM_SPN,
-                SubscriptionManager.NAME_SOURCE_CARRIER,
-                SubscriptionManager.NAME_SOURCE_USER_INPUT // user has highest priority.
-        ).indexOf(nameSource);
+        int index = Arrays.binarySearch(
+                new int[] {
+                        SubscriptionManager.NAME_SOURCE_DEFAULT_SOURCE,
+                        SubscriptionManager.NAME_SOURCE_SIM_PNN,
+                        SubscriptionManager.NAME_SOURCE_SIM_SPN,
+                        SubscriptionManager.NAME_SOURCE_CARRIER,
+                        SubscriptionManager.NAME_SOURCE_USER_INPUT // user has highest priority.
+                }, nameSource);
         return (index < 0) ? 0 : index;
     }
 
