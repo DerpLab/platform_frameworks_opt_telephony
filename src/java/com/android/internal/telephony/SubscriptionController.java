omnirom/android_frameworks_opt_telephony

Fix iccid issue during android upgrade.
Use fulliccid instead of stripped iccid so that the sub info
record matches.

Change-Id: Ic72c8b632ec691b4c6d99f70efb3168fd6bb653c
CRs-Fixed: 2281438

src/java/com/android/internal/telephony/SubscriptionController.java
@@ -921,6 +921,12 @@ public class SubscriptionController extends ISub.Stub {
      */
     @Override
     public int addSubInfoRecord(String iccId, int slotIndex) {
+        String fullIccId = iccId;
+        Phone phone = PhoneFactory.getPhone(slotIndex);
+        if (phone != null) {
+            fullIccId = phone.getFullIccSerialNumber();
+        }
+
         if (DBG) logdl("[addSubInfoRecord]+ iccId:" + SubscriptionInfo.givePrintableIccid(iccId) +
                 " slotIndex:" + slotIndex);
 
@@ -939,8 +945,10 @@ public class SubscriptionController extends ISub.Stub {
                     new String[]{SubscriptionManager.UNIQUE_KEY_SUBSCRIPTION_ID,
                             SubscriptionManager.SIM_SLOT_INDEX, SubscriptionManager.NAME_SOURCE,
                             SubscriptionManager.ICC_ID, SubscriptionManager.CARD_ID},
-                    SubscriptionManager.ICC_ID + "=?" + " OR " + SubscriptionManager.ICC_ID + "=?",
-                            new String[]{iccId, IccUtils.getDecimalSubstring(iccId)}, null);
+                    SubscriptionManager.ICC_ID + "=?" + " OR " + SubscriptionManager.ICC_ID + "=?"
+                             + " OR " + SubscriptionManager.ICC_ID + "=?" + " collate nocase",
+                            new String[]{iccId, IccUtils.getDecimalSubstring(iccId), fullIccId},
+                            null);
 
             boolean setDisplayName = false;
             try {
@@ -964,8 +972,9 @@ public class SubscriptionController extends ISub.Stub {
                         setDisplayName = true;
                     }
 
-                    if (oldIccId != null && oldIccId.length() < iccId.length()
-                            && (oldIccId.equals(IccUtils.getDecimalSubstring(iccId)))) {
+                    if (oldIccId != null && oldIccId.length() != iccId.length()
+                           && (oldIccId.equals(IccUtils.getDecimalSubstring(iccId))
+                                    || iccId.equals(IccUtils.stripTrailingFs(oldIccId)))) {
                         value.put(SubscriptionManager.ICC_ID, iccId);
                     }
 

DerpLab/platform_frameworks_opt_telephony

src/java/com/android/internal/telephony/SubscriptionController.java
@@ -1015,6 +1015,12 @@ public class SubscriptionController extends ISub.Stub {
     @Override
     public int addSubInfo(String uniqueId, String displayName, int slotIndex,
             int subscriptionType) {
+        String fullIccId = uniqueId;
+        Phone phone = PhoneFactory.getPhone(slotIndex);
+        if (phone != null) {
+            fullIccId = phone.getFullIccSerialNumber();
+        }
+
         if (DBG) {
             String iccIdStr = uniqueId;
             if (!isSubscriptionForRemoteSim(subscriptionType)) {
@@ -1042,8 +1048,9 @@ public class SubscriptionController extends ISub.Stub {
                 selection += " AND " + SubscriptionManager.SUBSCRIPTION_TYPE + "=?";
                 args = new String[]{uniqueId, Integer.toString(subscriptionType)};
             } else {
-                selection += " OR " + SubscriptionManager.ICC_ID + "=?";
-                args = new String[]{uniqueId, IccUtils.getDecimalSubstring(uniqueId)};
+                selection += " OR " + SubscriptionManager.ICC_ID + "=?"
+                    + " OR " + SubscriptionManager.ICC_ID + "=?" + " collate nocase";
+                args = new String[]{uniqueId, IccUtils.getDecimalSubstring(uniqueId), fullIccId};
             }
             Cursor cursor = resolver.query(SubscriptionManager.CONTENT_URI,
                     new String[]{SubscriptionManager.UNIQUE_KEY_SUBSCRIPTION_ID,
@@ -1081,8 +1088,9 @@ public class SubscriptionController extends ISub.Stub {
                             value.put(SubscriptionManager.SIM_SLOT_INDEX, slotIndex);
                         }
 
-                        if (oldIccId != null && oldIccId.length() < uniqueId.length()
-                                && (oldIccId.equals(IccUtils.getDecimalSubstring(uniqueId)))) {
+                        if (oldIccId != null && oldIccId.length() != uniqueId.length()
+                                && (oldIccId.equals(IccUtils.getDecimalSubstring(uniqueId))
+                                        || uniqueId.equals(IccUtils.stripTrailingFs(oldIccId)))) {
                             value.put(SubscriptionManager.ICC_ID, uniqueId);
                         }
 